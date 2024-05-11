975141e26a21cfe99c89cf5e614ebabcbcf9b46c
Anastasia Samilyk <61018491+asamilyk@users.noreply.github.com>
2024-05-11 16:16:25 +0200

    Create check_pin.c

---
+++ for_9_mark/check_pin.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+
+#define SHM_SIZE 1024
+#define SEM_NAME "/pin_semaphore"
+
+typedef struct {
+    int is_produced;
+} Pin;
+
+int main() {
+    // Открытие именованного семафора
+    sem_t *sem = sem_open(SEM_NAME, 0);
+    if (sem == SEM_FAILED) {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Открытие разделяемой памяти
+    int shm_fd = shm_open("/pin_memory", O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open");
+        exit(EXIT_FAILURE);
+    }
+    Pin *pins = mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (pins == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    // Проверка булавок
+    while (1) {
+        sem_wait(sem); // Ожидание доступа к разделяемой памяти
+        if (pins->is_produced) {
+            printf("Checked a pin.\n");
+            pins->is_produced = 0; // Сброс состояния булавки после проверки
+        }
+        sem_post(sem); // Освобождение доступа к разделяемой памяти
+        sleep(1); // Имитация времени проверки
+    }
+
+    // Закрытие семафора
+    sem_close(sem);
+
+    return 0;
+}
