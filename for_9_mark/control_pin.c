0d1b11b0f35a679f6484d0fd3484882c7c6747bb
Anastasia Samilyk <61018491+asamilyk@users.noreply.github.com>
2024-05-12 11:50:19 +0300

    Update control_pin.c

--- for_9_mark/control_pin.c
+++ for_9_mark/control_pin.c
@@ -2,45 +2,64 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <semaphore.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <time.h>
 
-#define SHM_SIZE 1024
-#define SEM_NAME "/pin_semaphore"
+#define SHARED_MEMORY_SIZE 1024
+#define SEMAPHORE_NAME "/my_semaphore"
 
 typedef struct {
-    int is_produced;
+    int is_curved;
+    int is_sharpened;
+    int is_checked;
 } Pin;
 
 int main() {
-    sem_t *sem = sem_open(SEM_NAME, 0);
+    // Открытие именованного семафора
+    sem_t *sem = sem_open(SEMAPHORE_NAME, O_CREAT, 0666, 1);
     if (sem == SEM_FAILED) {
         perror("sem_open");
         exit(EXIT_FAILURE);
     }
 
-    int shm_fd = shm_open("/pin_memory", O_RDWR, 0666);
-    if (shm_fd == -1) {
+    // Получение разделяемой памяти
+    int fd = shm_open("/my_shm", O_RDWR, 0666);
+    if (fd == -1) {
         perror("shm_open");
         exit(EXIT_FAILURE);
     }
-    Pin *pins = mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    Pin *pins = mmap(NULL, SHARED_MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (pins == MAP_FAILED) {
         perror("mmap");
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
-        sem_wait(sem);
-        if (pins->is_produced) {
-            printf("Quality controlled a pin.\n");
-            pins->is_produced = 0;
+    srand(time(NULL));
+
+    // Контроль качества булавок
+    for (int i = 0; i < SHARED_MEMORY_SIZE / sizeof(Pin); ++i) {
+        sem_wait(sem); // Ожидание доступа к разделяемой памяти
+        if (pins[i].is_sharpened) {
+            printf("Quality controlling pin %d...\n", i);
+            pins[i].is_checked = 1;
+            printf("Pin %d quality controlled.\n", i);
         }
-        sem_post(sem);
-        sleep(1);
+        sem_post(sem); // Освобождение доступа к разделяемой памяти
     }
 
-    sem_close(sem);
+    // Закрытие именованного семафора
+    if (sem_close(sem) == -1) {
+        perror("sem_close");
+        exit(EXIT_FAILURE);
+    }
+
+    // Отсоединение разделяемой памяти
+    if (munmap(pins, SHARED_MEMORY_SIZE) == -1) {
+        perror("munmap");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
